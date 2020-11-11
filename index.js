01988e0798b8ba0f7569befc1b6ee25fb25f828c
haydencarey <66346253+haydencarey@users.noreply.github.com>
2020-11-11 17:46:15 -0500

    glitch deploy

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.use('/', express.static('public'));
 //Initialize the actual HTTP server
 let http = require('http');
 let server = http.createServer(app);
-let port = process.env.PORT || 5000;
+let port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log("Server listening at port: " + port);
 });
